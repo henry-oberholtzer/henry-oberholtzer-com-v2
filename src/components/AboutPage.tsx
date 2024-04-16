e5026f031d58279c8391adaf89539c3bbe22e742
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-04-15 23:02:48 -0700

    update about page

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,9 +1,28 @@
-import ReactMarkdown from 'react-markdown'
-
 export default function AboutPage() {
 	return (
 		<div className="about-page">
-			<ReactMarkdown/>
+			<p>Programmer, musician & designer based in the Pacific Northwest</p>
+			<br/>
+			<h4>Recent Code</h4>
+			<p><a href="https://github.com/henry-oberholtzer/acidpattern">acidpattern</a> - Website for sharing patterns for the Roland TB-303.</p>
+			<p><a href="https://github.com/henry-oberholtzer/mp3-to-mp4">mp3-to-mp4</a> - Application to convert audio to mp4s for streaming platforms.</p>
+			<p><a href="https://github.com/henry-oberholtzer/zen-minesweeper">zen minesweeper</a> - Browser based recreation of the classic Windows 95 minesweeper.</p>
+			<p><a href="https://github.com/henry-oberholtzer/beyondtipping">beyondtipping</a> - Hub for finding restaurants that have moved away from tipping to living wages.</p>
+			<p><a href="https://github.com/henry-oberholtzer/repair-factory">repair factory</a> - Application for managing employees and resources with C# & MySQL</p>
+			<br />
+			<h4>Recent Music</h4>
+			<p><a href="https://hypersurfacesound.bandcamp.com/album/nippers-trip">Nipper's Trip</a> (2021) - Techno single written for my senior thesis at Reed College.</p>
+			<p><a href="https://renegadesnares.bandcamp.com/album/16171819">16171819</a> (2020) - A compilation of previous unreleased tracks from the vault</p>
+			<br />
+			<h4>Recent DJ Mixes</h4>
+			<p><a href="https://www.youtube.com/watch?v=lq_W5TR9Gcc&ab_channel=AcidArchivist">New Age of Acid Techno Volume II</a> (2024)</p>
+			<p><a href="https://youtu.be/iZMwH8TIqTc?si=0c2eV7BdjQOyHhOh">Vicious Circle Recordings</a> (2024)</p>
+			<p><a href="https://youtu.be/--fLlfPuzoo?si=mTKenOtnagyM-OL1">Breaking Through Ice</a> (2024)</p>
+			<p><a href="https://youtu.be/r2_pvBqQUmQ?si=wrrjIQIvcvHtr86L">Dope Bongos & Deep Dub Chords</a> (2024)</p>
+			<p><a href="https://youtu.be/iODhj8evf4I?si=kZNrB0-4-yTZIedY">Eurotrance Volume II - Cybercity</a> (2023)</p>
+			<br/>
+			<h4>Writings</h4>
+			<p><a href="https://github.com/henry-oberholtzer/waveform-transmissions/blob/main/thesis.pdf">Waveform Transmissions: Minimalism and Dynamic Expression in Techno</a> (2021)</p>
 		</div>
 	);
-};
+}
